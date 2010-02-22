365d032506a22fddef8ab01ac214295bf2913b5d
wallander <wallander.jonas@gmail.com>
2010-02-22 14:55:08 +0100

    project structure, first empty project

---
+++ trunk/src/com/android/towerdef/AndroidTD.java
@@ -0,0 +1,13 @@
+package com.android.towerdef;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidTD extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
