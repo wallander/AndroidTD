5c8913a1f42e4b4bbd9a3ee17d89ff316e665d50
wallander <wallander.jonas@gmail.com>
2010-02-23 16:14:18 +0100

    test push, only hello world, remove after success

--- trunk/src/com/android/towerdef/AndroidTD.java
+++ trunk/src/com/android/towerdef/AndroidTD.java
@@ -3,11 +3,21 @@ package com.android.towerdef;
 import android.app.Activity;
 import android.os.Bundle;
 
+// GIT TEST
+import android.widget.TextView;
+////
+
 public class AndroidTD extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // GIT TEST
+        TextView tv = new TextView(this);
+        tv.setText("git RULES!");
+        setContentView(tv);
+        ////
     }
 }
