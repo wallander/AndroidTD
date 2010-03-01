715b94af2c14b89a6bf9575fb091ee429912fd21
fredper <fredrik_persson_86@hotmail.com>
2010-03-01 15:38:21 +0100

    Pillat ännu mer med Mob.java

--- trunk/src/com/android/towerdef/Mob.java
+++ trunk/src/com/android/towerdef/Mob.java
@@ -9,56 +9,63 @@ import android.graphics.Bitmap;
  */
 public class Mob extends Unit{
 
-	private String mName;	// Mob name
-	private int mHealth;	// Mob health
-	private int mSpeed;	// Mob movement speed
-	private int mArmor;		// Mob armor
-	// private Coordinates mCoordinates; finns i Unit
+	/** Mob name */
+	private String mName;
 	
+	/** Mob health */
+	private int mHealth;
 	
-
+	/** Mob movement speed */
+	private int mSpeed;
+	
+	/** Mob armor */
+	private int mArmor;
+	
+	/** Mob type (Ground, air or invisible) */
+	private MobType mType;	
+	
+	/** Bitmap for the mob */
+    private Bitmap mBitmap;
+	
+	/**
+	 * Enum for the mob type. One for each type of mob.
+	 */
+	private enum MobType { AIR, GROUND, INVIS }
     
-    /**
-     * Bitmap which should be drawn.
+	
+	/**
+     * Constructor.
+     * 
+     * @param bitmap Bitmap which should be drawn.
      */
-    private Bitmap _bitmap;
+    public Mob(Bitmap pBitmap, MobType pType) {
+        mBitmap = pBitmap;
+        mType = pType;
+    }
+	
+    
 
 
-
-
-    /**
-     * Object type which could be rock, scissors, paper or explosion.
-     */
-    private String _type;
 
     /**
      * Step of explosion which will take 50 steps.
      */
     private int _explosionStep = 0;
 
-    /**
-     * Constructor.
-     * 
-     * @param bitmap Bitmap which should be drawn.
-     */
-    public Mob(Bitmap bitmap) {
-        _bitmap = bitmap;
-       // _coordinates = new Coordinates();
-       // mSpeed = new Speed();
-    }
+    
 
     /**
      * @param bitmap New bitmap to draw.
      */
-    public void setBitmap(Bitmap bitmap) {
-        _bitmap = bitmap;
+    public void setBitmap(Bitmap pBitmap) {
+        mBitmap = pBitmap;
     }
 
     /**
      * @return The stored bitmap.
      */
     public Bitmap getBitmap() {
-        return _bitmap;
+        return mBitmap;
     }
 
     /**
@@ -73,28 +80,16 @@ public class Mob extends Unit{
     /**
      * @param type The new type of the instance.
      */
-    public void setType(String type) {
-        _type = type;
+    public void setType(MobType pType) {
+        mType = pType;
     }
 
     /**
      * @return The type of the instance.
      */
-    public String getType() {
-        return _type;
+    public MobType getType() {
+        return mType;
     }
 
-    /**
-     * @param step The new explosion step.
-     */
-    public void setExplosionStep(int step) {
-        _explosionStep = step;
-    }
-
-    /**
-     * @return The explosion step.
-     */
-    public int getExplosionStep() {
-        return _explosionStep;
-    }
+   
 }
