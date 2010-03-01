8bbe82580cad10f3bec614111bb28128bb2782cf
wallander <wallander.jonas@gmail.com>
2010-03-01 16:12:33 +0100

    removed invalid reference in GamePanel

--- trunk/src/com/chalmers/game/td/GamePanel.java
+++ trunk/src/com/chalmers/game/td/GamePanel.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 import java.util.Random;
 
-import com.android.towerdef.R;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
