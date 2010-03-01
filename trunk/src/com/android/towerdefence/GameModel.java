0ea14f0d4f4c9c149f30c2b8ea6837c9da002dd1
sunkas85 <sunkas@gmail.com>
2010-03-01 14:07:09 +0100

    Test

---
+++ trunk/src/com/android/towerdefence/GameModel.java
@@ -0,0 +1,16 @@
+package com.android.towerdefence;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.graphics.Bitmap;
+import android.media.SoundPool;
+
+public class GameModel {
+
+	
+
+    // Insert objects here... typ...
+	
+}
