9609902a6a653f5a382c9d924022e90dfe0e64ce
wallander <wallander.jonas@gmail.com>
2010-03-10 17:09:08 +0100

    Added a new method in MobFactory.java, it returns a single mob for every call untill there are no more mobs, then it returns null.

--- trunk/src/com/chalmers/game/td/MobFactory.java
+++ trunk/src/com/chalmers/game/td/MobFactory.java
@@ -2,6 +2,7 @@ package com.chalmers.game.td;
 
 import java.util.List;
 
+import com.chalmers.game.td.exceptions.WaveNotInitializedException;
 import com.chalmers.game.td.initializers.WaveLoader;
 import com.chalmers.game.td.units.Mob;
 
@@ -22,25 +23,45 @@ public class MobFactory {
 	private static final MobFactory	INSTANCE = new MobFactory();
 	private WaveLoader				mWaveLoader;
 	private List<List<Mob>>			mWaves;
+	private List<Mob>				mMobs;
 	
 	/**
 	 * Should not be used, call getInstance() instead.
 	 */
 	private MobFactory() {
-		
+		mWaveLoader = null;
+		mWaves = null;		
+		mMobs = null;
 	}
 	
 	/**
-	 * Returns a list containing the different waves of mobs, represented by a List<Mob>.
 	 * 
-	 * @param pLevel
 	 * @return
 	 */
-	public List<List<Mob>> getWaves(int pLevel) {
+	public Mob getMob() {
+				
+		Mob mMob;
+		
+		if(mMobs != null) {
+			mMob = mMobs.remove(0);
+		} else if(mWaves != null) {
+			mMobs = mWaves.remove(0);
+		} else {
+			return null;
+		}
+		
+		return mMob;
+	}
+	
+	/**
+	 * 
+	 * @param pLevel
+	 */
+	public void initWaves(int pLevel) {
 		
 		mWaveLoader = new WaveLoader("init/waves/level" + pLevel);
 		mWaves = mWaveLoader.getWaves();
-		return mWaves;
+		mMobs = mWaves.remove(0);
 	}
 		
 	/**
