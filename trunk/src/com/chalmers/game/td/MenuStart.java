eab835ae4c67576a4b23e3656c76576b3225d7c8
Ahmed <ahmed.chaban@gmail.com>
2010-03-17 10:22:29 +0100

    menu system

---
+++ trunk/src/com/chalmers/game/td/MenuStart.java
@@ -0,0 +1,55 @@
+package com.chalmers.game.td;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+
+public class MenuStart extends Activity {
+    protected boolean _active = true;
+    protected int _splashTime = 10000;
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+        
+        // Set the screen orientation to Portrait
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        
+        // thread for displaying the SplashScreen
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while(_active && (waited < _splashTime)) {
+                        sleep(100);
+                        if(_active) {
+                            waited += 100;
+                        }
+                    }
+                } catch(InterruptedException e) {
+                    // do nothing
+                } finally {
+                    finish();
+                    startActivity(new Intent("com.dotted.games.Menu"));
+                    stop();
+                }
+            }
+        };
+        splashTread.start();
+    }
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            _active = false;
+        }
+        return true;
+    }
+}
