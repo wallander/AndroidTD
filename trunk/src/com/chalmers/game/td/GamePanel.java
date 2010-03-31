88ee7fec0bd25f7b16d0954e293bb42e4d5a45f8
fredper <fredrik_persson_86@hotmail.com>
2010-03-31 11:59:32 +0200

    fixat lite allt möjligt (ingenting)

--- trunk/src/com/chalmers/game/td/GamePanel.java
+++ trunk/src/com/chalmers/game/td/GamePanel.java
@@ -230,6 +230,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     public boolean onTouchEvent(MotionEvent event) {
     	
     	
+    	
         synchronized (getHolder()) {
             
         	
