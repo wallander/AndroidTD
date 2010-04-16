86da3990633c28b8aef66dbec354271d891850e3
sunkas85 <sunkas@gmail.com>
2010-04-16 12:49:49 +0200

    fontändring

--- trunk/gen/com/chalmers/game/td/R.java
+++ trunk/gen/com/chalmers/game/td/R.java
@@ -9,11 +9,13 @@ package com.chalmers.game.td;
 
 public final class R {
     public static final class array {
-        public static final int mobs_track_1=0x7f050003;
-        public static final int mobs_track_2=0x7f050004;
+        public static final int mobs_track_1=0x7f050005;
+        public static final int mobs_track_2=0x7f050006;
         public static final int path_track_1=0x7f050000;
         public static final int path_track_2=0x7f050001;
         public static final int path_track_3=0x7f050002;
+        public static final int path_track_4=0x7f050003;
+        public static final int path_track_5=0x7f050004;
     }
     public static final class attr {
     }
