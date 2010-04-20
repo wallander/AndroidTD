1d89c234c1ebcf19a56d2cd6255e0ae21a255ed3
wallander <wallander.jonas@gmail.com>
2010-04-20 08:21:21 +0200

    removed highscore.xml, will use file writing instead

--- trunk/gen/com/chalmers/game/td/R.java
+++ trunk/gen/com/chalmers/game/td/R.java
@@ -9,21 +9,16 @@ package com.chalmers.game.td;
 
 public final class R {
     public static final class array {
-        public static final int mobs_track_1=0x7f05000a;
-        public static final int mobs_track_2=0x7f05000b;
-        public static final int mobs_track_3=0x7f05000c;
-        public static final int mobs_track_4=0x7f05000d;
-        public static final int mobs_track_5=0x7f05000e;
-        public static final int path_track_1=0x7f050005;
-        public static final int path_track_2=0x7f050006;
-        public static final int path_track_3=0x7f050007;
-        public static final int path_track_4=0x7f050008;
-        public static final int path_track_5=0x7f050009;
-        public static final int score_track_1=0x7f050000;
-        public static final int score_track_2=0x7f050001;
-        public static final int score_track_3=0x7f050002;
-        public static final int score_track_4=0x7f050003;
-        public static final int score_track_5=0x7f050004;
+        public static final int mobs_track_1=0x7f050005;
+        public static final int mobs_track_2=0x7f050006;
+        public static final int mobs_track_3=0x7f050007;
+        public static final int mobs_track_4=0x7f050008;
+        public static final int mobs_track_5=0x7f050009;
+        public static final int path_track_1=0x7f050000;
+        public static final int path_track_2=0x7f050001;
+        public static final int path_track_3=0x7f050002;
+        public static final int path_track_4=0x7f050003;
+        public static final int path_track_5=0x7f050004;
     }
     public static final class attr {
     }
