9e3ea12ef66a9a406aced268b5898ee43e246980
wallander <wallander.jonas@gmail.com>
2010-04-22 16:38:14 +0200

    merge fixes

--- trunk/src/com/chalmers/game/td/Player.java
+++ trunk/src/com/chalmers/game/td/Player.java
@@ -1,16 +1,12 @@
 package com.chalmers.game.td;
 
-import android.util.Log;
-
 import com.chalmers.game.td.units.Mob;
 
 public class Player {
 
 	private String 			mName;
 	private final double	STARTING_MONEY = 400;
-	private double 			mMoney;
-	private double			mCurrentTrackScore;	
-	
+	private double 			mMoney;	
 	private Highscore		mScore;
 	
 	private int mLives = 50;
@@ -20,13 +16,7 @@ public class Player {
 		setMoney(STARTING_MONEY);		
 		mScore = Highscore.getInstance();
 		mScore.setTracks(pTracks);
-		boolean b = mScore.loadScore();
-		
-		if(b) {
-			Log.v("PLAYER KONSTRUKTOR", "Lyckades ladda från fil");
-		} else {
-			Log.v("PLAYER KONSTRUKTOR", "Lyckades INTE ladda från fil");
-		}
+		mScore.loadScore();
 	}		
 	
 	public void changeScore(Mob pMob) {
